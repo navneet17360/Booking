86a4570bb1f7b29ef6ec3cf5f935ea6c8ac8a1b0
navneet17360 <navneetanand.2021cs@technonjr.org>
2024-03-07 22:46:35 +0530

    authentication done in hotels

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,24 +7,34 @@ import {
   getUserById,
   updateUser,
 } from "../controllers/userController.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-  res.status(200).json("hello , you are logged in");
-});
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.status(200).json("hello,you are logged in");
+// });
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res
+//     .status(200)
+//     .json("hello user,you are logged in and you can delete your account");
+// });
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res
+//     .status(200)
+//     .json("hello user,you are logged in and you can delete all account");
+// });
 
 // UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 // DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 // GET
-router.get("/:id", getUserById);
+router.get("/:id", verifyUser, getUserById);
 
 // GET ALL
-router.get("/", getAllUsers);
+router.get("/", verifyAdmin, getAllUsers);
 
 export default router;
