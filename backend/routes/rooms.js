16d669c308e94d6a157188c3f7869754f028a49d
navneet17360 <navneetanand.2021cs@technonjr.org>
2024-03-08 12:11:40 +0530

    roomController,Room Model,room route

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,3 +1,26 @@
 import express from "express";
+import Room from "../models/Room.js";
+import { createError } from "../utils/error.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import {
+  createRoom,
+  deleteRoom,
+  getAllRooms,
+  getRoomById,
+  updateRoom,
+} from "../controllers/roomController.js";
 const router = express.Router();
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+//UPDATE
+
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
+router.get("/:id", getRoomById);
+//GET ALL
+router.get("/", getAllRooms);
+
 export default router;
