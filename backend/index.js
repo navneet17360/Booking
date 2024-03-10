9a041a6257e86387dd42c313e1e4d16312c46ea7
navneet17360 <navneetanand.2021cs@technonjr.org>
2024-03-10 15:39:23 +0530

    useFetch hook is applied in Featured Component using proxy

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
-
+import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
@@ -24,6 +24,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,53 +1,65 @@
+import useFetch from "../../hooks/useFetch";
 import "./featured.css";
+// import { useEffect, useState } from "react";
 
 const Featured = () => {
+  const { data, loading, error } = useFetch(
+    "/api/hotels/countByCity?cities=Mumbai,Delhi,Hyderabad,Chennai"
+  );
+  console.log(data);
   return (
     <div>
       <div className="featured">
-        <div className="featuredItem">
-          <img
-            src="https://cf.bstatic.com/xdata/images/city/600x600/684534.jpg?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o="
-            className="featuredImage"
-            alt=""
-          />
-          <div className="featuredTitles">
-            <h1>Jaipur</h1>
-            <h2>123 properties</h2>
-          </div>
-        </div>
-        <div className="featuredItem">
-          <img
-            src="https://cf.bstatic.com/xdata/images/city/600x600/684765.jpg?k=3f7d20034c13ac7686520ac1ccf1621337a1e59860abfd9cbd96f8d66b4fc138&o="
-            className="featuredImage"
-            alt=""
-          />
-          <div className="featuredTitles">
-            <h1>New Delhi</h1>
-            <h2>123 properties</h2>
-          </div>
-        </div>
-        <div className="featuredItem">
-          <img
-            src="https://cf.bstatic.com/xdata/images/city/600x600/971346.jpg?k=40eeb583a755f2835f4dcb6900cdeba2a46dc9d50e64f2aa04206f5f6fce5671&o="
-            className="featuredImage"
-            alt=""
-          />
-          <div className="featuredTitles">
-            <h1>Mumbai</h1>
-            <h2>123 properties</h2>
-          </div>
-        </div>
-        <div className="featuredItem">
-          <img
-            src="https://cf.bstatic.com/xdata/images/city/600x600/684653.jpg?k=306ccafcc8a4a7e23b9e8a05b183453fe885b312a4daa5ce76ec39a1b79cbc6f&o="
-            className="featuredImage"
-            alt=""
-          />
-          <div className="featuredTitles">
-            <h1>Hyderabad</h1>
-            <h2>123 properties</h2>
-          </div>
-        </div>
+        {loading ? (
+          "Loading please wait"
+        ) : (
+          <>
+            <div className="featuredItem">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/600x600/684534.jpg?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o="
+                className="featuredImage"
+                alt=""
+              />
+              <div className="featuredTitles">
+                <h1>Mumbai</h1>
+                <h2>{data[0]} Properties</h2>
+              </div>
+            </div>
+            <div className="featuredItem">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/600x600/684765.jpg?k=3f7d20034c13ac7686520ac1ccf1621337a1e59860abfd9cbd96f8d66b4fc138&o="
+                className="featuredImage"
+                alt=""
+              />
+              <div className="featuredTitles">
+                <h1>Delhi</h1>
+                <h2>{data[1]} Properties</h2>
+              </div>
+            </div>
+            <div className="featuredItem">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/600x600/684730.jpg?k=e37b93d88c1fe12e827f10c9d6909a1def7349be2c68df5de885deaa4bc01ee3&o="
+                className="featuredImage"
+                alt=""
+              />
+              <div className="featuredTitles">
+                <h1>Chennai</h1>
+                <h2>{data[2]} Properties</h2>
+              </div>
+            </div>
+            <div className="featuredItem">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/600x600/684653.jpg?k=306ccafcc8a4a7e23b9e8a05b183453fe885b312a4daa5ce76ec39a1b79cbc6f&o="
+                className="featuredImage"
+                alt=""
+              />
+              <div className="featuredTitles">
+                <h1>Hyderabad</h1>
+                <h2>{data[3]} Properties</h2>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
